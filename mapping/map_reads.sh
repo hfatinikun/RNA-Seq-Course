2063b8e2301bdc69cf2ea67da8ce36e8f6a490b6
Heritage Fatinikun <heritage.fatinikun@students.unibe.ch>
2024-11-24 00:34:15 +0100

    Map reads to the reference genome

---
+++ mapping/map_reads.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --array=1-12
+#SBATCH --mail-type=fail
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=data/users/hfatinikun/output_%j.o
+#SBATCH --error=data/users/hfatinikun/error_%j.o
+#SBATCH --mail-user=heritage.fatinikun@students.unibe.ch
+#SBATCH --time=13:00:00
+#SBATCH --mem=64G
+#SBATCH --partition=pibu_el8
+#SBATCH --job-name="RNA Seq Mapping Reads"
+
+#Define Variables
+SAMPLELIST='/data/users/hfatinikun/RNA-Seq-Course/quality_checks/samplelist.txt'
+APPTAINER_IMG='/containers/apptainer/hisat2_samtools_408dfd02f175cd88.sif'
+INDEX_BASENAME='/data/users/hfatinikun/RNA-Seq-Course/Reference_genome/genome_index'
+INDEX_DIR='/data/users/hfatinikun/RNA-Seq-Course/Reference_genome'
+READS_DIR="/data/courses/rnaseq_course/breastcancer_de/reads"
+MAPPED_READS='/data/users/hfatinikun/RNA-Seq-Course/mapping' #Path to save mapped reads
+
+#Paths to the Reads samplenames for the mapping
+SAMPLE=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $1; exit}' $SAMPLELIST`
+READ1=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $2; exit}' $SAMPLELIST`
+READ2=`awk -v line=$SLURM_ARRAY_TASK_ID 'NR==line{print $3; exit}' $SAMPLELIST`
+
+#map reads
+apptainer exec --bind /data/ $APPTAINER_IMG hisat2 -x $INDEX_BASENAME -1 $READ1 -2 $READ2 -S $MAPPED_READS/$SAMPLE.sam -p 4
+
+BAM_DIR='/data/users/hfatinikun/RNA-Seq-Course/mapping/BAM_Files'
+mkdir -p $BAM_DIR
+
+echo "Converting SAM to BAM for $SAMPLE">&2
+#Convert SAM to BAM
+apptainer exec --bind /data/ $APPTAINER_IMG samtools view -hbS $MAPPED_READS/$SAMPLE.sam > $BAM_DIR/$SAMPLE"_unsorted.bam"
+
+echo "Sorting bam file for $SAMPLE">&2
+#Sorting the BAM Files
+apptainer exec --bind /data/ $APPTAINER_IMG samtools sort -m 35G -@ 4 -o $BAM_DIR/$SAMPLE.bam -T temp $BAM_DIR/$SAMPLE"_unsorted.bam"
+
+echo "Indexing $SAMPLE.bam">&2
+#Indexing the sorted BAM Files
+apptainer exec --bind /data/ $APPTAINER_IMG samtools index $BAM_DIR/$SAMPLE.bam
+
+echo "Mapping of read completed"
---
+++ mapping/reference_and_index.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#SBATCH --mail-type=fail
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --output=data/users/hfatinikun/output_%j.o
+#SBATCH --error=data/users/hfatinikun/error_%j.o
+#SBATCH --mail-user=heritage.fatinikun@students.unibe.ch
+#SBATCH --time=13:00:00
+#SBATCH --mem=8G
+#SBATCH --partition=pibu_el8
+#SBATCH --job-name="RNA Seq Download and Index Ref Genome "
+
+#Define Variables
+REF_GENOME_LINK='https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz'
+REF_ANNOTATION_LINK='https://ftp.ensembl.org/pub/release-113/gtf/homo_sapiens/Homo_sapiens.GRCh38.113.gtf.gz'
+REFERENCE_DIR='/data/users/hfatinikun/RNA-Seq-Course/Reference_genome'
+HISAT2_IMG='/containers/apptainer/hisat2_samtools_408dfd02f175cd88.sif'
+GENOME_CHECKSUM_LINK='https://ftp.ensembl.org/pub/release-113/fasta/homo_sapiens/dna/CHECKSUMS'
+ANNOTATION_CHECKSUM_LINK='https://ftp.ensembl.org/pub/release-113/gtf/homo_sapiens/CHECKSUMS'
+
+reference_genome=$(basename $REF_GENOME_LINK)
+reference_annotation=$(basename $REF_ANNOTATION_LINK)
+
+mkdir -p $REFERENCE_DIR
+cd $REFERENCE_DIR
+
+#Download the latest reference genome and associated annotation from the Ensembl ftp site
+wget $REF_GENOME_LINK
+wget $REF_ANNOTATION_LINK 
+
+# Download checksum file without saving it -> get the line corresponding to the files we use -> keep the 2 first columns
+genome_expected_checksum=$(wget -O - $GENOME_CHECKSUM_LINK | grep $reference_genome | awk '{print $1,$2}')
+annotation_expected_checksum=$(wget -O - $ANNOTATION_CHECKSUM_LINK | grep $reference_annotation | awk '{print $1,$2}')
+
+# Testing if checksums calculated on downloaded files are as expected
+if [ "$genome_expected_checksum" != "$(sum $reference_genome)" ] || \
+    [ "$annotation_expected_checksum" != "$(sum $reference_annotation)" ]
+then 
+    # throw an error and exit
+    echo "Checksums are not as expected !">&2
+    rm $reference_genome
+    rm $reference_annotation 
+    exit 1
+fi
+
+# Unzipping the reference genome
+if [[ "$reference_genome" == *.gz ]]
+then
+    echo "Unzipping the reference genome..."
+    gunzip $reference_genome
+    reference_genome=${reference_genome%.*}         # After unzipping, remove the .gz extension from the file name
+fi
+
+# Indexing the reference genome
+apptainer exec --bind /data/ $HISAT2_IMG hisat2-build $REFERENCE_DIR/$reference_genome $REFERENCE_DIR/genome_index
+
+echo "RNA Seq reference genome preparation complete."
